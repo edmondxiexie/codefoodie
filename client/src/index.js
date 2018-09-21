a6ae288f5545942df7857c76745af2ace37cbe9b
edmondxiexie <xiejy36@gmail.com>
2018-09-20 22:45:28 -0700

    Fix client side router.

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import path from 'path';
 
 import App from './components/App';
 import './style/index.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const NavBar = () => {
+  return <div>NavBar!</div>;
+};
+
+const Footer = () => {
+  return <div>Footer!</div>;
+};
+
+const Home = () => {
+  return <div>Home!</div>;
+};
+
+const Detail = () => {
+  return <div>Detail!</div>;
+};
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path="/navbar" component={NavBar} />
+      <Route path="/footer" component={Footer} />
+    </div>
+  </BrowserRouter>,
+  document.getElementById('app')
+);
