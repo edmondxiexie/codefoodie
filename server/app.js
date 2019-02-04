936345a3a8d567250552e62ced805a208116ecac
edmondxiexie <xiejy36@gmail.com>
2019-02-04 02:30:23 -0800

    Update node env

--- server/app.js
+++ server/app.js
@@ -34,6 +34,8 @@ app.use('/api/recipe', recipeRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/num', numRoutes);
 
+console.log(`****** NODE_ENV: ${process.env.NODE_ENV} ******`);
+
 if (process.env.NODE_ENV === 'production') {
   console.log(`****** NODE_ENV: ${process.env.NODE_ENV} ******`);
   // production assets
